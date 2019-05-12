f93feef91f52ad8885a1dcbe74704aca515ba077
Oreooo <yuan80056@gmail.com>
2019-05-13 00:15:25 +0800

    优化代码

--- app/src/main/java/com/example/oreooo/wanandroidtest/MainActivity.java
+++ app/src/main/java/com/example/oreooo/wanandroidtest/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.oreooo.wanandroidtest;
 
 import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.oreooo.wanandroidtest.wanAndroid.WanAndroidFragment;
 import com.oreooo.library.MvpBase.BaseActivity;
 
 public class MainActivity extends BaseActivity {
-    WanAndroidFragment mWanAndroidFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
